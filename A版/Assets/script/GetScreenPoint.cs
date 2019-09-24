02874eedac476c6b07d03d6295bba035468bf0b6
Ryutatsu89287 <55589680+Ryutatsu89287@users.noreply.github.com>
2019-09-25 01:24:22 +0900

    Tue

---
+++ A版/Assets/script/GetScreenPoint.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetScreenPoint : MonoBehaviour
+{
+    public Camera maincamera;
+    Vector3 TopLeft;
+    Vector3 TopRight;
+    Vector3 BottomLeft;
+    Vector3 BottomRight;
+    public float width;
+    public float height;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //GetScreenTopLeft();
+        //GetScreenTopRight();
+        //GetScreenBottomRight();
+        //GetScreenBottomLeft();
+        //Debug.Log("TopLeft");
+        //Debug.Log(TopLeft);
+        //Debug.Log("TopRight");
+        //Debug.Log(TopRight);
+        //Debug.Log("BottomRight");
+        //Debug.Log(BottomRight);
+        //Debug.Log("BottomLeft");
+        //Debug.Log(BottomLeft);
+    }
+
+
+    //画面の左上の位置座標を返す
+    public Vector3 GetScreenTopLeft()
+    {
+        TopLeft = maincamera.ScreenToWorldPoint(Vector3.zero);
+        return TopLeft;
+    }
+
+    public Vector3 GetScreenTopRight()
+    {
+        TopRight = maincamera.ScreenToWorldPoint(new Vector3(Screen.width, 0.0f, 0.0f));
+        return TopRight;
+    }
+
+    //画面の右下の位置座標を返す
+    public Vector3 GetScreenBottomRight()
+    {
+        BottomRight = maincamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0.0f));
+        return BottomRight;
+    }
+
+    //画面の右下の位置座標を返す
+    public Vector3 GetScreenBottomLeft()
+    {
+        BottomLeft = maincamera.ScreenToWorldPoint(new Vector3(0.0f, Screen.height, 0.0f));
+        return BottomRight;
+    }
+}
---
+++ A版/Assets/script/TileSwitch.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileSwitch : MonoBehaviour
+{
+    private GameObject obj;
+    GetScreenPoint PointScript;
+    Vector3 CameraTopLeft;
+    Vector3 CameraTopRight;
+    Vector3 CameraBottomLeft;
+    Vector3 CameraBottomRight;
+    Vector3 ViewSize;
+    public float ObjectScale;
+    Vector3 TileMax;
+    Vector2 ReturnPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        obj = GameObject.Find("GameManeger");
+        PointScript = obj.GetComponent<GetScreenPoint>();
+        CameraTopLeft = PointScript.GetScreenTopLeft();
+        CameraTopRight = PointScript.GetScreenTopRight();
+        CameraBottomLeft = PointScript.GetScreenBottomLeft();
+        CameraBottomRight = PointScript.GetScreenBottomRight();
+        GetViewSize();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    //入力された数をfloat型の座標に変換
+    Vector2 TileAllocation(int Numx, int Numy)
+    {
+        Vector2 StartPoint;
+        StartPoint.x = -((GetViewSize().x / 2) + (ObjectScale / 2));
+        StartPoint.y = -((GetViewSize().y / 2)+ (ObjectScale/2));
+        ReturnPos.x = (StartPoint.x * Numx);
+        ReturnPos.y = (StartPoint.y * Numy);
+        return ReturnPos;
+    }
+
+    // タイルの最大数
+    Vector2 GetTileMax()
+    {
+        TileMax.x = (ViewSize.x / ObjectScale); //x個
+        TileMax.y = (ViewSize.y / ObjectScale); //y個
+        return TileMax;
+    }
+
+    //画面の大きさを返す
+    Vector2 GetViewSize()
+    {
+        ViewSize.x = (CameraTopRight.x - CameraTopLeft.x);
+        ViewSize.y = (CameraBottomLeft.y - CameraTopLeft.y);
+        return ViewSize;
+
+        for (int x = 0; x < TileMax.x; x++)
+        {
+            for (int y = 0; y < TileMax.y; y++)
+            {
+
+            }
+        }
+    }
+     
+}
